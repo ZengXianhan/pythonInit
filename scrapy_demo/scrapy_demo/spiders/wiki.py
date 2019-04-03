ae7a81d447003ea22b93376769371f6ff98bfe38
DESKTOP-933BD3A\曾宪涵 <zengxianhan93@gmail.com>
2019-04-03 22:27:17 +0800

    Xpath DEMO

--- scrapy_demo/scrapy_demo/spiders/wiki.py
+++ scrapy_demo/scrapy_demo/spiders/wiki.py
@@ -1,12 +1,22 @@
 # -*- coding: utf-8 -*-
 import scrapy
-
+import re
 
 class WikiSpider(scrapy.Spider):
+    base_url = 'https://zh.wikipedia.org/'
     name = 'wiki'
-    allowed_domains = ['https://zh.wikipedia.org/']
-    start_urls = ['http://https://zh.wikipedia.org//']
+    allowed_domains = [base_url]
+    start_urls = [base_url + 'wiki/Python']
 
-    print("111")
     def parse(self, response):
+        title_selector = response.xpath('//*[@id="firstHeading"]/text()')
+        href_selector = response.xpath("//@href")
+        title = title_selector.extract()[0]
+        hrefs = href_selector.extract()
+        wiki_hrefs = set()
+        for href in hrefs:
+            wiki_href = re.match("/wiki/*", href)
+            if(wiki_href is not None):
+                wiki_hrefs.add(self.base_url + wiki_href.string)
+
         pass
